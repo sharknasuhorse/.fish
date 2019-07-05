ca275d316c339513b8d0b69d9dafbbc22a367db8
sharknasuhorse <nakaryo6459shark@gmail.com>
2019-07-05 14:58:54 +0900

    add

---
+++ config.fish
@@ -0,0 +1,6 @@
+set -x LSCOLORS gxfxcxdxbxegedabagacad
+alias airport=/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport
+set PATH $PATH /sbin /usr/sbin ~/Documents/scripts/ ~/.nodebrew/current/bin /Library/Internet Plug-Ins/JavaAppletPlugin.plugin/Contents/Home/bin
+set PATH $HOME/.pyenv/shims $PATH
+eval (pyenv init - | source)
+
